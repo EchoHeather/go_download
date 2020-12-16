8936d450ef791877d6e7bc5aaaeeef0766927953
Heather <2428792711@qq.com>
2020-12-16 13:49:19 +0800

    mysql

---
+++ db/mysql/conn.go
@@ -0,0 +1,26 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+)
+
+var db *sql.DB
+
+//init mysql初始化
+func init() {
+	db, _ := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/fileserver?charset=uft8")
+	db.SetMaxOpenConns(1000)
+	err := db.Ping()
+	if err != nil {
+		fmt.Println("Failed to connect to mysql, err : " + err.Error())
+		os.Exit(1)
+	}
+}
+
+//DBConn 返回数据库对象实例,内部是基于连接池实现的，因此不用过于担心并发的问题
+func DBConn() *sql.DB {
+	return db
+}
