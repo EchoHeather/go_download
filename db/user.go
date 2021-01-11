92be3e918bce36d1faea78d9f6b5a109b9a6c575
Heather <2428792711@qq.com>
2021-01-11 18:05:47 +0800

    登录

--- db/user.go
+++ db/user.go
@@ -46,12 +46,25 @@ func UserSignIn(username, password string) bool {
 	}
 
 	pRows := mydb.ParseRows(rows)
-	if len(pRows) > 0 && string(pRows[0]["user_pwd"].([]byte)) == password{
+	if len(pRows) > 0 && string(pRows[0]["user_pwd"].([]byte)) == password {
 		return true
 	}
 	return false
 }
 
-func PrepareRows() {
+//UpdateToken 刷新用户登录token
+func UpdateToken(username, token string) bool {
+	stmt, err := mydb.DBConn().Prepare("replace into tbl_user_token(`user_name`, `user_token`) VALUES (?, ?)")
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	defer stmt.Close()
 
+	_, err = stmt.Exec(username, token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	return true
 }
--- handler/user.go
+++ handler/user.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"fmt"
 	dblayer "goWork/db"
 	"goWork/util"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 const pwd_salt = "*#890"
@@ -47,9 +49,40 @@ func SignInHandler(w http.ResponseWriter, r *http.Request) {
 		encPassWd := util.Sha1([]byte(password + pwd_salt))
 
 		//验证账号密码
-		dblayer.UserSignIn(username, encPassWd)
+		pwdChecked := dblayer.UserSignIn(username, encPassWd)
 
+		if !pwdChecked {
+			w.Write([]byte("FAILED"))
+			return
+		}
+		//生成token
+		token := GenToken(username)
+		upRes := dblayer.UpdateToken(username, token)
+		if !upRes {
+			w.Write([]byte("FAILED"))
+			return
+		}
 		//登陆成功并跳转
-
+		resp := util.RespMsg{
+			Code: 0,
+			Msg:  "OK",
+			Data: struct {
+				Location string
+				Username string
+				Token    string
+			}{
+				Location: "http://" + r.Host + "/static/view/home.html",
+				Username: username,
+				Token:    token,
+			},
+		}
+		w.Write(resp.JSONBytes())
 	}
+}
+
+//GenToken 获取token 40位字符md5(username + timestamp + token_salt) + timestamp[:8]
+func GenToken(username string) string {
+	ts := fmt.Sprintf("%x", time.Now().Unix())
+	tokenPrefix := util.MD5([]byte(username + ts + "_tokensalt"))
+	return tokenPrefix + ts[:8]
 }
